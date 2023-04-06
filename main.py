5225231954e7b461a6994d144f593faf2b6509e8
ingslh <ingslh@foxmail.com>
2023-04-06 22:56:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,21 @@
 from dotenv import load_dotenv
-import os
-import base64
-from requests import post, get
-import json
-
-import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-
 from imdata import ImData
+from urllib import request, parse
+import os
+import spotipy
 
+def search_for_track(sp, track_name, artist_name):
+  search_ret = sp.search(track_name, 10)
+  search_tracks = search_ret["tracks"]["items"]
+  for track in search_tracks:
+    test_name  = track["name"]
+    test_artist = track["artists"][0]["name"]
+    if track["name"].lower() == track_name.lower() and track["artists"][0]["name"].lower() == artist_name.lower() :
+       return True , track["uri"]
 
-load_dotenv()
+  return False, ""
 
-client_id = os.getenv("SPOTIPY_CLIENT_ID")
-client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
-
-print(client_id, client_secret)
-
-def rep(s):
-    li = []
-    for i in s:
-        li.append(i)
-    for i in range(len(li)):
-        if li[i] == ' ':
-            li[i] = '%20'
-    return ''.join(li)
-
-def get_token():
-  auth_string = client_id + ":" + client_secret
-  auth_bytes = auth_string.encode("utf-8")
-  auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
-
-  url = "https://accounts.spotify.com/api/token"
-  headers = {
-    "Content-Type"  : "application/x-www-form-urlencoded"
-  }
-  data = "grant_type=client_credentials&client_id=" + client_id + "&client_secret=" + client_secret
-  print(data)
-  result = post(url, headers = headers, data = data)
-  json_result = json.loads(result.content)
-  token = json_result["access_token"]
-  return token
-
-def get_auth_header(token):
-  return {"Authorization": "Bearer  " + token}
-
-def search_for_track(token, track_name, artist_name):
-  url =  "https://api.spotify.com/v1/search"
-  headers = get_auth_header(token)
-  pre_query = f"q=remaster track:{track_name} artist:{artist_name}&type=track&limit=1"
-  #pre_query = f"?q={track_name}&type:track"
-  query = rep(pre_query)
-  query_url = url + "?" +  query 
-  result = get(query_url, headers = headers)
-  json_result = json.loads(result.content)
-  print(json_result)
 
 def creat_sp():
   scope = "playlist-modify-public"
@@ -65,24 +26,47 @@ def creat_sp():
 def create_a_playlist(sp, playlist_name):
   user_id = sp.me()['id']
   ret = sp.user_playlist_create(user_id, playlist_name)
-  return ret
+  return ret["id"]
 
-def add_items_to_playlist(sp, playlist_uri, track_uris):
-  sp.playlist_add_items(playlist_uri, track_uris)
+def add_items_to_playlist(sp, playlist_uri, track_uris, position=None):
+  plid = sp._get_id("playlist", playlist_uri)
+  ftracks = [sp._get_uri("track", tid) for tid in [track_uris]]
+  return sp._post(
+         "playlists/%s/tracks" % (plid),
+          payload=ftracks,
+          position=position,
+  )
 
 def main():
+
+  load_dotenv()
+
+  client_id = os.getenv("SPOTIPY_CLIENT_ID")
+  client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
+  redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
+
+  print(client_id, client_secret)
+
   tracks_info = ImData.read_imdata()
   sp = creat_sp()
   playlist_name = "test1"
-  result = create_a_playlist(sp, playlist_name)
-  playlist_id = json_result = json.loads(result.content)
 
-  token = get_token()
-  search_for_track(token, "Doxy", "Miles Davis")
+  ser_id = sp.me()['id']
+  playlist_id = create_a_playlist(sp, playlist_name)
 
+  tracks_count = len(tracks_info)
+  cnt = 0
   for track in tracks_info:
-    ret = search_for_track(token, track["track_name"], track["artist_name"])
-    add_items_to_playlist(playlist_id, )
+    ret,track_uri = search_for_track(sp, track["track_name"], track["artist"])
+    if ret :
+      response = add_items_to_playlist(sp, playlist_id, track_uri)
+      if "snapshot_id" in response:
+        cnt += 1
+        print(f"Import to \"{playlist_id}\"({cnt} / {tracks_count}) : " + track["track_name"] + " - " + track["artist"])
+
 
 if __name__ == '__main__':
   main()
+
+
+
